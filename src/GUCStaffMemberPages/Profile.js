fa2ed1beba6778448c54ee61571c6ede718eb29b
Abdelaziz Adel <abdelazizadel99@gmail.com>
2021-01-08 00:52:16 +0200

    added logout component

--- src/GUCStaffMemberPages/Profile.js
+++ src/GUCStaffMemberPages/Profile.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Logout from './Logout';
+import {Redirect} from 'react-router-dom';
 
 class Profile extends React.Component {
-
     render() {
         return (
-            <h1>{this.props.userId}</h1>
+            this.props.userId === "" ? <Redirect to="/"/> :
+            <>
+                <h1>{this.props.userId}</h1>
+                <Logout setUserId={this.props.setUserId} userId={this.props.userId}/>
+            </>
         );
     }
 }
