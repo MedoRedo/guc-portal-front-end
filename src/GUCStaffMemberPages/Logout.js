a163d482dc188d3bd8de463792f1181051af6fb0
Abdelaziz Adel <abdelazizadel99@gmail.com>
2021-01-08 18:17:38 +0200

    added change password page & converted id state to local storage entry

--- src/GUCStaffMemberPages/Logout.js
+++ src/GUCStaffMemberPages/Logout.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import axios from 'axios';
-
+import { useHistory } from 'react-router';
 
 function Logout(props) {
+
+  let history = useHistory();
     
   const handleClick = async (e) => {
     try {
-      await axios.get('http://localhost:5000/logout');
-      props.setUserId("");
-      
+      await axios.get('http://localhost:5000/logout',{
+        headers : {
+          auth_token : localStorage.getItem('auth_token')
+        }
+      });
+      localStorage.removeItem('auth_token'); 
+      localStorage.removeItem('userId');
+      history.push('/');     
     }
     catch(e) {
       console.log(e);
--- src/GUCStaffMemberPages/Profile.js
+++ src/GUCStaffMemberPages/Profile.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import Logout from './Logout';
-import {Redirect} from 'react-router-dom';
+import {Redirect, Link} from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 
-class Profile extends React.Component {
-    render() {
-        return (
-            this.props.userId === "" ? <Redirect to="/"/> :
-            <>
-                <h1>{this.props.userId}</h1>
-                <Logout setUserId={this.props.setUserId} userId={this.props.userId}/>
-            </>
-        );
-    }
+
+function Profile(props) {
+  return (
+    localStorage.getItem('auth_token') === null ? <Redirect to="/"/> :
+    <>
+      <h1>{localStorage.getItem('userId')}</h1>
+      <Logout/>
+      <Link to="/changePassword" style={{textDecoration:'none'}}>
+        <Button
+          variant="contained"
+          color="secondary"
+        >
+          Change Password
+        </Button>
+      </Link>
+    </>
+  );
 }
+
 
 export default Profile;
