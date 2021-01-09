28df332dde65575032f6554966ae538c1729e6aa
Abdelaziz Adel <abdelazizadel99@gmail.com>
2021-01-09 12:44:07 +0200

    removed unused imports and optimized useEffect call in navBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React from 'react';
-import Login from './GUCStaffMemberPages/Login';
-import Profile from './HODPages/HODProfile';
-import Requests from "./HODPages/Requests";
-import Courses from "./HODPages/Courses";
-import ChangePassword from './GUCStaffMemberPages/ChangePassword';
 import { Route, Switch } from 'react-router-dom';
-import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 function App() {
   return (
