0551d6287e4a33ba74b3b48f5c9f22c4c5c5d2f8
Mazen <mazen.ashraf.12@gmail.com>
2021-01-10 03:18:49 +0200

    added Instructor courses component

---
+++ src/InstructorPages/CoursesPage.js
@@ -0,0 +1,66 @@
+import React, {useState, useEffect} from 'react'
+import { makeStyles } from "@material-ui/core/styles";
+import axios from 'axios';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Typography from '@material-ui/core/Typography';
+import { Button } from '@material-ui/core';
+import {useHistory} from 'react-router-dom'
+const useStyles = makeStyles((theme) => ({
+    title: {margin: '5px'},
+    table: {margin: 'auto'},
+    cell :{textAlign:'center'}
+}));
+
+const InstructorCourses = (props) => {
+    const classes = useStyles();
+    const history = useHistory();
+    const [courses, setCourses] = useState([]);
+    useEffect(() => {
+        // console.log('componentDidMount');
+        axios.get("http://localhost:5000/instructors/courses",{
+            headers : {
+              auth_token : localStorage.getItem('auth_token')
+            }
+          }).then(res => {
+            console.log(res.data)
+            setCourses(
+                res.data.courses
+            );                    
+        });        
+    },[]);
+
+    const handleClick = (id) => {
+        history.push(`instructors/courses/${id}`);
+    }
+    return(courses.length!=0&&<>
+        <Typography component="h2" variant="h6" color="primary" className={classes.title} align='center'>
+            Instructor Courses
+        </Typography>
+        <Table size="medium" className={classes.table}>
+            <TableHead>
+                <TableRow>
+                    <TableCell className={classes.cell}></TableCell>
+                    <TableCell className={classes.cell}>Course ID</TableCell>
+                    <TableCell className={classes.cell}>Course Name</TableCell>
+                </TableRow>
+            </TableHead>
+
+            <TableBody>
+                {courses.map(course => {
+                    return <TableRow key={course.courseId}>
+                        <TableCell className={classes.cell}><Button variant='outlined' color='primary' onClick={() => handleClick(course.courseId)}>View Course</Button></TableCell>
+                        <TableCell className={classes.cell}>{course.courseId}</TableCell>
+                        <TableCell className={classes.cell}>{course.courseName}</TableCell>
+                    </TableRow>
+                })}
+            </TableBody>
+        </Table>
+    </>
+    ); 
+}
+
+export default InstructorCourses;
