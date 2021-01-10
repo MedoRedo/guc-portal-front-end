f3f7b44e6f528aaa556ef76b04b7808f5aa80301
morejas <54472095+morejas@users.noreply.github.com>
2021-01-10 13:35:30 +0200

    editing the title in the department courses page

--- src/HODPages/Course.js
+++ src/HODPages/Course.js
@@ -1,6 +1,21 @@
-import React from "react";
+import axios from "axios";
+import React,{useEffect, useState} from "react";
 
-export default function course(props) {
-    console.log(props);
-    return(<div>{props.course}</div>)
+
+
+export default function Course(props) {
+    const[course, setCourse] = useState();
+    useEffect(()=> {
+        axios.get(`http://localhost:5000/HOD/courses/${props.match.params.id}`,{
+            headers : {
+              auth_token : localStorage.getItem('auth_token')
+            }//, params: {page: props.match.params.id}
+          })
+        .then(res =>{
+            // console.log(res.data);
+            setCourse(res.data)
+        })
+    }, [])
+//    console.log(course.numSlots);
+    return(<div>{course !== undefined && course.mainDepartment}</div>)
 }
