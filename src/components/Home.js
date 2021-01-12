8116540996fdb4ff5155de10ac0b8855a32b5fe3
Mazen <mazen.ashraf.12@gmail.com>
2021-01-12 03:27:06 +0200

    added the routes for course info page and its redirects'

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Login from '../GUCStaffMemberPages/Login';
 import Profile from '../GUCStaffMemberPages/Profile';
 import Requests from "../HODPages/Requests";
-import AllCourses from '../CommonCourses/Courses'
+import AllCourses from '../CommonCourses/Courses';
+import InstructorCourseInfo from '../InstructorPages/CourseInfoPage'
 import Course from "../HODPages/Course";
 import ChangePassword from '../GUCStaffMemberPages/ChangePassword';
 import { Route, Switch } from 'react-router-dom';
 import NavBar from "./NavBar";
+import ViewProfile from '../InstructorPages/ViewProfilePage';
+import AssignCoordinator from '../InstructorPages/CoordinatorAssigningPage';
+import InstructorSlots from '../InstructorPages/SlotsPage';
 
 function Home() {
   return (
@@ -18,6 +22,10 @@ function Home() {
         <Route path="/courses/:id?" render={props => <Course {...props}/>} exact/>
         <Route path="/requests" component={Requests} exact/>
         <Route path="/changePassword" component={ChangePassword} exact/>
+        <Route path="/courses/:id/instructor" component={InstructorCourseInfo} exact/>;
+        <Route path="/profile/:id" component={ViewProfile} exact/>
+        <Route path="/course/:courseId/CoordinatorAssigning" component={AssignCoordinator} exact />
+        <Route path="/course/:courseId/slots" component={InstructorSlots} exact />
         <Route path="" render={props => <Profile {...props}/>} exact/>
 
       </Switch>
