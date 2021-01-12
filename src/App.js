5c55cca8a73875a2e76a204643cf03f59ecbf1a3
Ahmed Rezik <ahmedrezq100@hotmail.com>
2021-01-12 15:10:39 +0200

    Created Location Views

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from "./components/Home";
+import Profile from "./HODPages/AssignInstructor";
+import CourseCard from "./components/CourseCard";
+import AddLocation from "./HRpages/Add Location";
+import LocationOps from "./HRpages/LocationOps";
+
 function App() {
   return (
-    <Route path="/" component={Home} />
+    <Route path="/" component={LocationOps} />
   );
 }
 
