7c40138ae5b3fa089b8c2887cdf3ef8721e24b16
Mazen <mazen.ashraf.12@gmail.com>
2021-01-15 15:07:42 +0200

    added the coordinator courses in the courses page

--- src/CommonCourses/Courses.js
+++ src/CommonCourses/Courses.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import InstructorCourses from '../InstructorPages/CoursesPage';
 import  DepartmentCourses from "../HODPages/DepartmentCourses";
+import CoordinatorCourses from '../CoordinatorPages/CoordinatorCourses';
 const AllCourses = (props)=>{
     return (<>
         <DepartmentCourses/>
         <InstructorCourses/>
+        <CoordinatorCourses/>
+
     </>)
 }
 export default AllCourses;
